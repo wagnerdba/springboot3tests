8e5edb21a20f2f5d348427dfa52f75f6d7700853
wagnerdba <wagnerdba@gmail.com>
2024-02-14 11:51:51 -0300

    Dto and mapper created

---
+++ src/main/java/com/wrtecnologia/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package com.wrtecnologia.ems.mapper;
+
+import com.wrtecnologia.ems.dto.EmployeeDto;
+import com.wrtecnologia.ems.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+
+}
